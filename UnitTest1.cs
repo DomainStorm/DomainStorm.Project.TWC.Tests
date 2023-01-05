a02fac1af55d9e7821d824f308b0034a2df2eb75
Yan <yanshibainu@gmail.com>
2023-01-05 14:24:59 +0800

    init

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DomainStorm.Project.TWC.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
