628b5bd9e390881a3c97792e1f6e4dde827ab03c
Yan <yanshibainu@gmail.com>
2023-06-12 10:42:51 +0800

    test

--- deploy/deploy-dev.ps1
+++ deploy/deploy-dev.ps1
@@ -29,36 +29,4 @@ $env:ServiceBus_Version = "0.0.4"
 
 docker compose -f docker-compose.yml -f docker-compose.metadataapi.yml -f docker-compose.dev.yml up -d
 
-$containers = @("multimediaapi", "metadataapi", "jwtauthapi", "openidprovider", "twcweb", "resourceapi", "servicebus")
-
-while ($true) {
-    $current_time = Get-Date
-    $elapsed_time = ($current_time - $start_time).TotalSeconds().ToString()
-
-    if ($elapsed_time -ge $timeout) {
-        Write-Host "Container health check timeout"
-        exit 1
-    }
-
-    $all_containers_healthy = $true
-
-    foreach ($container in $containers) {
-        $container_health = docker inspect --format='{{json .State.Health.Status}}' $container
-
-        if ($container_health -ne "healthy") {
-            $all_containers_healthy = $false
-            Write-Host "Container $container is not healthy"
-            break
-        }
-    }
-
-    if ($all_containers_healthy) {
-        Write-Host "All containers are healthy"
-        break
-    }
-
-    Write-Host "Waiting for containers to become healthy..."
-    Start-Sleep -Seconds 5
-}
-
 ./deploy-seed-pipeline-dev.ps1
