2cf6241aaa57973f978ec15e9a74452926121c57
hana1029 <410411324@gms.nduh.edu.tw>
2023-07-11 16:36:24 +0800

    Test0.1.5 先確保資料夾存在後再下載

--- TwcC101Tests.cs
+++ TwcC101Tests.cs
@@ -639,10 +639,6 @@ namespace DomainStorm.Project.TWC.Tests
 
             actions.MoveToElement(夾帶附件).Click().Perform();
 
-            IWebElement 下載PDF = driver.FindElement(By.CssSelector("button.btn.bg-gradient-warning.m-0.ms-2"));
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", 下載PDF);
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", 下載PDF);
-
             string downloadsFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
             if (!Directory.Exists(downloadsFolderPath))
             {
@@ -650,6 +646,10 @@ namespace DomainStorm.Project.TWC.Tests
             }
 
             Console.WriteLine("downloadsFolderPath: " + downloadsFolderPath);
+
+            IWebElement 下載PDF = driver.FindElement(By.CssSelector("button.btn.bg-gradient-warning.m-0.ms-2"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", 下載PDF);
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", 下載PDF);
 
             string filePath = Path.Combine(downloadsFolderPath, "41101699338.pdf");
             Console.WriteLine("filePath: " + filePath);
