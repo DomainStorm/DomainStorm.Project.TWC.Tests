4f4ec61dd101ebd240c359999eb694f3a40ac9f9
hana1029 <410411324@gms.nduh.edu.tw>
2023-08-09 10:41:21 +0800

    Test

--- TwcM100Tests.cs
+++ TwcM100Tests.cs
@@ -296,7 +296,6 @@ namespace DomainStorm.Project.TWC.Tests
             上傳.Click();
             Thread.Sleep(1000);
 
-            //Console.WriteLine(driver.PageSource);
             stormCard = driver.FindElement(By.CssSelector("storm-card"));
             IWebElement stormEditTable = stormCard.FindElement(By.CssSelector("storm-edit-table"));
             IWebElement stormTable = stormEditTable.GetShadowRoot().FindElement(By.CssSelector("storm-table"));
