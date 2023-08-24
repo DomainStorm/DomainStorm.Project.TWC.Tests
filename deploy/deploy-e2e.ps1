ba8ed8c5b320cd5df9f8493e030e6eb4ac361c5a
hana1029 <410411324@gms.nduh.edu.tw>
2023-08-24 10:12:19 +0800

    new_test E201

--- deploy/deploy-e2e.ps1
+++ deploy/deploy-e2e.ps1
@@ -23,11 +23,9 @@ $env:MetadataApi_Version = "1.1.1"
 $env:MultiMediaApi_Version = "0.0.6"
 $env:JwtAuthApi_Version = "0.3.1"
 $env:OpenidProvider_Version = "0.3.1"
-
-$env:TwcWeb_Version = "0.9.2"
-
+$env:TwcWeb_Version = "0.9.3"
 $env:ResourceApi_Version = "0.1.3"
-$env:ServiceBus_Version = "0.0.4"
+$env:ServiceBus_Version = "0.0.5"
 
 docker compose -f docker-compose.yml -f docker-compose.metadataapi.yml -f docker-compose.e2e.yml up -d
 
