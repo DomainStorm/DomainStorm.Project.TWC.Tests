936c257676982993b449211cb576d171226a487f
hana1029 <410411324@gms.nduh.edu.tw>
2023-09-04 10:48:48 +0800

    Debug

--- TwcS100Tests.cs
+++ TwcS100Tests.cs
@@ -462,7 +462,7 @@ namespace DomainStorm.Project.TWC.Tests
             SelectElement selectMonth = new SelectElement(monthDropdown);
             selectMonth.SelectByText("March");
 
-            var spanElement = driver.FindElement(By.CssSelector("span[aria-label='March 5, 2023']"));
+            var spanElement = driver.FindElement(By.CssSelector("span[aria-label='March 1, 2023']"));
             spanElement.Click();
 
             var divElement = stormCard.FindElement(By.CssSelector("div.d-flex.justify-content-end.mt-4"));
