e78f5926597b849a5aef82513b25694306f24f3f
yanshibainu <yanshibainu@gmail.com>
2023-10-11 18:26:20 +0800

    $env:TwcWeb_Version = "1.0.1" $env:TwcReport_Version = "0.0.9"

--- deploy/deploy-e2e.ps1
+++ deploy/deploy-e2e.ps1
@@ -23,10 +23,10 @@ $env:MetadataApi_Version = "1.1.1"
 $env:MultiMediaApi_Version = "0.0.6"
 $env:JwtAuthApi_Version = "0.3.3"
 $env:OpenidProvider_Version = "0.3.3"
-$env:TwcWeb_Version = "0.9.9"
+$env:TwcWeb_Version = "1.0.1"
 $env:ResourceApi_Version = "0.1.4"
 $env:ServiceBus_Version = "0.0.6"
-$env:TwcReport_Version = "0.0.8"
+$env:TwcReport_Version = "0.0.9"
 
 docker compose -f docker-compose.yml -f docker-compose.metadataapi.yml -f docker-compose.e2e.yml up -d
 
