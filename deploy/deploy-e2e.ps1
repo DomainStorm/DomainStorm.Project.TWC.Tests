b6c78b0e1d909a896047f729b1b2d4f14f7768fc
hana1029 <410411324@gms.nduh.edu.tw>
2023-10-12 15:51:02 +0800

    update

--- deploy/deploy-e2e.ps1
+++ deploy/deploy-e2e.ps1
@@ -26,7 +26,7 @@ $env:OpenidProvider_Version = "0.3.3"
 $env:TwcWeb_Version = "1.0.1"
 $env:ResourceApi_Version = "0.1.4"
 $env:ServiceBus_Version = "0.0.6"
-$env:TwcReport_Version = "0.0.8"
+$env:TwcReport_Version = "0.0.9"
 
 docker compose -f docker-compose.yml -f docker-compose.metadataapi.yml -f docker-compose.e2e.yml up -d
 
