9a1edda58d3c482d0a6f02e5a1c0dc0139136664
hana1029 <410411324@gms.nduh.edu.tw>
2023-10-20 16:35:49 +0800

    Debug value抓不到換id

--- TwcRA001Tests.cs
+++ TwcRA001Tests.cs
@@ -287,7 +287,8 @@ namespace DomainStorm.Project.TWC.Tests
             var 區處別 = _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("storm-card > form > div.mt-3 > storm-select >div.choices")));
             _actions.MoveToElement(區處別).Click().Perform();
 
-            var 第四區管理處 = _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("div.choices__list.choices__list--dropdown > div.choices__list > [data-value='3eed4fc4-9c06-4d16-9eb6-45aeaf198a25']")));
+            //var 第四區管理處 = _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("div.choices__list.choices__list--dropdown > div.choices__list > [data-value='3eed4fc4-9c06-4d16-9eb6-45aeaf198a25']")));
+            var 第四區管理處 = _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("div.choices__list.choices__list--dropdown > div.choices__list > [data-id='2']")));
             _actions.MoveToElement(第四區管理處).Click().Perform();
 
             // 選擇受理日期起
