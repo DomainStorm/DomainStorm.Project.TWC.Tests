e02cf19a475a317606b471a939b0b5cec15f2799
hana1029 <410411324@gms.nduh.edu.tw>
2023-11-02 11:13:50 +0800

    重構

--- TwcB101Tests.cs
+++ TwcB101Tests.cs
@@ -88,7 +88,15 @@ namespace DomainStorm.Project.TWC.Tests
 
             var uploadButton = _wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.d-flex.justify-content-end.mt-4 button[name='button']")));
             _actions.MoveToElement(uploadButton).Click().Perform();
+            That(TestHelper.WaitUploadCompleted(_driver), Is.Not.Null);
 
+            var stormEditTable = _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("storm-edit-table")));
+            var stormTable = stormEditTable.GetShadowRoot().FindElement(By.CssSelector("storm-table"));
+            var deleteStormButton = stormTable.GetShadowRoot().FindElement(By.CssSelector("storm-button"));
+            _actions.MoveToElement(deleteStormButton).Click().Perform();
+
+            var spanTitle = stormTable.GetShadowRoot().FindElement(By.CssSelector("storm-table-cell > span"));
+            That(spanTitle.Text, Is.EqualTo("twcweb_01_1_夾帶附件2.pdf"));
 
             //    var stormCardSeventh = stormVerticalNavigation.FindElements(By.CssSelector("storm-card"))[6];
             //    var stormEditTable = stormCardSeventh.FindElement(By.CssSelector("storm-edit-table"));
