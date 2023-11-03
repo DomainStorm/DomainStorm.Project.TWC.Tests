95e23695bf5f320a136a60ea9924d717eb70c889
hana1029 <410411324@gms.nduh.edu.tw>
2023-11-03 17:37:59 +0800

    Debug

--- TwcD101Tests.cs
+++ TwcD101Tests.cs
@@ -129,8 +129,9 @@ namespace DomainStorm.Project.TWC.Tests
         }
         public async Task TwcD101_08()
         {
-            Thread.Sleep(3000);
+            //Thread.Sleep(3000);
             var infoButton = TestHelper.FindAndMoveElement(_driver, "button.btn.bg-gradient-info.m-0.ms-2");
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("button.btn.bg-gradient-info.m-0.ms-2")));
             infoButton.Click();
 
             _wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.CssSelector("button.btn.bg-gradient-info.m-0.ms-2")));
