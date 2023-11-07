613814a3013c40aa28c3f8c825f2f986a0a65210
hana1029 <410411324@gms.nduh.edu.tw>
2023-11-07 11:12:39 +0800

    秒數增加

--- TwcC101Tests.cs
+++ TwcC101Tests.cs
@@ -22,7 +22,7 @@ namespace DomainStorm.Project.TWC.Tests
         public void Setup()
         {
             _driver = TestHelper.GetNewChromeDriver();
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
             _actions = new Actions(_driver);
         }
 
