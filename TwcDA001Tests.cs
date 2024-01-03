b0b8d8bb062cbe794ba1f0da03323b6db5f28eb9
hana1029 <410411324@gms.nduh.edu.tw>
2024-01-03 11:52:26 +0800

    新增DA001

---
+++ TwcDA001Tests.cs
@@ -0,0 +1,108 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Net;
+using static NUnit.Framework.Assert;
+
+namespace DomainStorm.Project.TWC.Tests
+{
+    public class TwcDA001Tests
+    {
+        private IWebDriver _driver = null!;
+        private WebDriverWait _wait = null!;
+        private Actions _actions = null!;
+        public TwcDA001Tests()
+        {
+            TestHelper.CleanDb();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = TestHelper.GetNewChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            _actions = new Actions(_driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        [Order(0)]
+        public async Task TwcDA001_01To12()
+        {
+            await TwcDA001_01();
+            await TwcDA001_02();
+            await TwcDA001_03();
+            await TwcDA001_04();
+            await TwcDA001_05();
+            await TwcDA001_06();
+            await TwcDA001_07();
+            await TwcDA001_08();
+            await TwcDA001_09();
+        }
+        public async Task TwcDA001_01()
+        {
+            TestHelper.AccessToken = await TestHelper.GetAccessToken();
+            That(TestHelper.AccessToken, Is.Not.Empty);
+        }
+        public async Task TwcDA001_02()
+        {
+            HttpStatusCode statusCode = await TestHelper.CreateForm(TestHelper.AccessToken!, $"{TestHelper.BaseUrl}/api/v1/bmTransferApply/confirm", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets/twcweb-DA001_bmTransferApply.json"));
+            That(statusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+        public async Task TwcDA001_03()
+        {
+            await TestHelper.Login(_driver, "0511", TestHelper.Password!);
+            _driver.Navigate().GoToUrl($@"{TestHelper.BaseUrl}/draft");
+            TestHelper.ClickRow(_driver, TestHelper.ApplyCaseNo!);
+
+            _wait.Until(ExpectedConditions.ElementExists(By.CssSelector("iframe")));
+
+            var href = TestHelper.FindShadowRootElement(_driver, "[href='#finished']");
+            _actions.MoveToElement(href).Click().Perform();
+
+
+        }
+        public async Task TwcDA001_04()
+        {
+
+        }
+        public async Task TwcDA001_05()
+        {
+
+        }
+        public async Task TwcDA001_06()
+        {
+
+        }
+        public async Task TwcDA001_07()
+        {
+
+        }
+        public async Task TwcDA001_08()
+        {
+
+        }
+        public async Task TwcDA001_09()
+        {
+
+        }
+        public async Task TwcDA001_10()
+        {
+
+        }
+        public async Task TwcDA001_11()
+        {
+
+        }
+        public async Task TwcDA001_12()
+        {
+          
+        }
+    }
+}
