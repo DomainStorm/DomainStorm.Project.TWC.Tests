a31f72a7429dd30ca616b89a496687f12267d272
Yan <yanshibainu@gmail.com>
2024-09-30 14:52:24 +0800

    test

--- TestHelper.cs
+++ TestHelper.cs
@@ -431,7 +431,8 @@ public class TestHelper
                     if (rows.Count == 1)
                     {
                         var row = rows[0];
-                        var applyCaseNoElement = row.FindElement(By.CssSelector("td[data-field='applyCaseNo']"));
+
+                        var applyCaseNoElement = _wait.Until(_ => row.FindElement(By.CssSelector("td[data-field='applyCaseNo']")));
                         if (applyCaseNoElement.Text == caseNo)
                             return true;
                     }
@@ -448,8 +449,8 @@ public class TestHelper
         {
             var rows = stormTable.GetShadowRoot().FindElements(By.CssSelector("tbody > tr"));
 
-            return rows.Select(row => row.FindElement(By.CssSelector("td[data-field='applyCaseNo']")))
-                .FirstOrDefault(applyCaseNoElement => applyCaseNoElement.Text == caseNo);
+            return rows.Select(row => _wait.Until(_ => row.FindElement(By.CssSelector("td[data-field='applyCaseNo']"))))
+                .FirstOrDefault(webElement => webElement.Text == caseNo);
         });
 
         //var selectedRow = rows.FirstOrDefault(tr =>
